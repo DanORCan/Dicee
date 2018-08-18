32453d12eff752f8d28fb0715bbb8116cc8da4b3
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2018-08-18 14:23:33 -0400

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by DANIEL OREILLY on 8/18/18.
+//  Copyright © 2018 DANIEL OREILLY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

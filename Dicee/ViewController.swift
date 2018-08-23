d970c1f7c62b34b865e6f25337c70c9bb1cf4101
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2018-08-23 16:27:55 -0400

    Modified image on buttons and used new random number function for Swift 4.2.

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var randomDiceIndex1 : Int = 0
-    var randomDiceIndex2 : Int = 0
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
     
 
     @IBOutlet weak var diceImageView1: UIImageView!
@@ -24,11 +26,14 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        randomDiceIndex1 = Int(arc4random_uniform(6))
-        randomDiceIndex2 = Int(arc4random_uniform(6))
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
         
         print(randomDiceIndex1)
         print(randomDiceIndex2)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
     
 }
